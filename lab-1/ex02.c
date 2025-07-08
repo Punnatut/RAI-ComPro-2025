aeb8a93508674f637b53031cf17502e57926b2be
Punnatut <68011494@kmitl.ac.th>
2025-07-08 05:36:58 +0000

    assignment1-5

---
+++ lab-1/ex02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is : %s\n","Pun");
+    printf("I am : %d\n years old ",18 );
+    printf("I am : %f cm", 182.0);
+
+return 0;
+}
---
+++ lab-1/ex05.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\'This is a single quoted string'");
+
+    return 0;
+}
+
