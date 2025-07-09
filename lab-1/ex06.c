b69205b583623ab7f795d06347449a942366d0c0
Punnatut <68011494@kmitl.ac.th>
2025-07-09 23:17:07 +0700

    ex06.c

---
+++ lab-1/ex06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 35;
+    
+    printf("Decimal: %d\n",num);
+    printf("Octal: %o\n",num);
+    printf("Hexadecimal: %x\n",num);
+    
+    return 0;
+}
