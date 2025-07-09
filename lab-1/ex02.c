018e9095c3950db8cef37bcf4d5f5eb5cb726072
Punnatut <68011494@kmitl.ac.th>
2025-07-09 23:50:44 +0700

    ex02.c

--- lab-1/ex02.c
+++ lab-1/ex02.c
@@ -3,7 +3,7 @@
 int main()
 {
     printf("My name is : %s\n","Pun");
-    printf("I am : %d\n years old ",18 );
+    printf("I am : %d years old \n ",18 );
     printf("I am : %f cm", 182.0);
 
 return 0;
